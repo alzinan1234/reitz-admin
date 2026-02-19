084b4faac28a09d5f25e9d00848273d17b791d54
alzinan1234 <pappurohan1100@gmail.com>
2026-02-19 22:35:26 +0600

    added

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -13,10 +13,12 @@ const geistSans = Geist({
   subsets: ['latin'],
 });
 
+
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
 });
+
 
 
 export default function RootLayout({ children }) {
